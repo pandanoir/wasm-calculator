48d8d7b8cdc6cfa96116d586c8aca58a03f8aea2
pandanoir <moon_in_france@yahoo.co.jp>
2023-04-09 19:47:49 +0900

    prohibit invalid integer such as 000

--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -2,35 +2,39 @@ extern crate combine;
 use combine::parser::char::{char, digit, spaces};
 use combine::{many, ParseError, Parser, Stream, *};
 
-// <expr>   ::= <term> [ ('+'|'-') <term> ]*
-// <term>   ::= <factor> [ ('*'|'/') <factor> ]*
-// <factor> ::= <number> | '(' <expr> ')'
-// <number> :== 1つ以上の数字
-
 trait ExprParser<Input: Stream<Token = char>, Output>: Parser<Input, Output = Output>
 where
     Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
 }
-impl<Input: Stream<Token = char>, Output, T> ExprParser<Input, Output> for T where
-    T: Parser<Input, Output = Output>
+impl<Input: Stream<Token = char>, Output, T: Parser<Input, Output = Output>>
+    ExprParser<Input, Output> for T
 {
 }
 
+// <expr>   ::= <term> [ ('+'|'-') <term> ]*
+// <term>   ::= <factor> [ ('*'|'/') <factor> ]*
+// <factor> ::= <number> | '(' <expr> ')'
+// <number> :== 1つ以上の数字
 fn lex_char<Input: Stream<Token = char>>(c: char) -> impl ExprParser<Input, char> {
     let skip_spaces = || spaces().silent();
     char(c).skip(skip_spaces())
 }
 
 fn number<Input: Stream<Token = char>>() -> impl ExprParser<Input, f64> {
-    let num = || many1(digit()).map(|x: String| x.parse::<f64>().unwrap());
+    let sign = optional(lex_char('-')).map(|x| if let Some(_) = x { -1.0 } else { 1.0 });
+    let integer = char('0')
+        .map(|_| 0.0_f64)
+        .or((one_of("123456789".chars()), many(digit()))
+            .map(|x: (char, String)| (x.0.to_string() + &x.1).parse::<f64>().unwrap()));
+    let decimal =
+        many1(digit()).map(|x: String| x.parse::<f64>().unwrap() * 10_f64.powf(-(x.len() as f64)));
     (
-        optional(lex_char('-')).map(|x| if let Some(_) = x { -1.0 } else { 1.0 }),
-        num(),
-        optional((char('.'), num().map(|x| x * 10_f64.powf(-x.log10().ceil()))))
-            .map(|x| x.map(|n| n.1)),
+        sign,
+        integer,
+        optional((char('.'), decimal)).map(|x| x.map(|n| n.1).unwrap_or(0.0)),
     )
-        .map(|(sign, int, frac)| sign * (int + frac.unwrap_or(0.0)))
+        .map(|(sign, int, frac)| sign * (int + frac))
 }
 fn factor<Input: Stream<Token = char>>() -> impl ExprParser<Input, f64> {
     between(lex_char('('), lex_char(')'), expr()).or(number())
--- rust/tests/web.rs
+++ rust/tests/web.rs
@@ -10,6 +10,9 @@ wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn pass() {
+    assert_eq!(calc("0.1"), Ok(0.1));
+    assert_eq!(calc("0.0001"), Ok(0.0001));
+    assert_eq!(calc("10.0001"), Ok(10.0001));
     assert_eq!(calc("3.2"), Ok(3.2));
     assert_eq!(calc("-3.2"), Ok(-3.2));
     assert_eq!(calc("1.2+3.4*5.6"), Ok(20.24));
@@ -19,8 +22,11 @@ fn pass() {
     assert_eq!(calc("3*(2*2)"), Ok(12.0));
     assert_eq!(calc("-3*(2*2)"), Ok(-12.0));
     assert_eq!(calc("(64)"), Ok(64.0));
-    assert_eq!(calc("123+456"),Ok(579.0));
+    assert_eq!(calc("123+456"), Ok(579.0));
     assert_eq!(calc("1+2*(3+4)"), Ok(15.0));
     assert_eq!(calc("12/4"), Ok(3.0));
     assert_eq!(calc("12-4"), Ok(8.0));
+
+    assert_eq!(calc("00.1"), Err(String::from("invalid formula: 0.1")));
+    assert_eq!(calc("0.1.2.3"), Err(String::from("invalid formula: .2.3")));
 }
