32f379bcdd2de523e854ac15d5f9f10cadeb9536
pandanoir <moon_in_france@yahoo.co.jp>
2023-04-09 19:57:46 +0900

    remove unused lines

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,15 +1,9 @@
 mod utils;
-extern crate combine;
 
 use utils::parse_expr;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
 pub fn calc(expr: &str) -> Result<f64, String> {
-  parse_expr(expr)
+    parse_expr(expr)
 }
